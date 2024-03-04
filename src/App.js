1e03d8d8303072136d7c903fcaef561d6884de58
smartfusion01 <itsealonge@yahoo.com>
2024-03-04 13:03:45 +0100

    save to local storage, delete and add to favourite functions

--- src/App.js
+++ src/App.js
@@ -1,43 +1,86 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+import AddFavourites from "./components/AddFavourites";
+import RemoveFavourites from "./components/RemoveFavourites";
 
 const App = () => {
-  const [movies, setMovies] = useState([
-    {
-      "Title": "Star Wars: Episode IV - A New Hope",
-      "Year": "1977",
-      "imdbID": "tt0076759",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg"
-    },
-    {
-      "Title": "Star Wars: Episode V - The Empire Strikes Back",
-      "Year": "1980",
-      "imdbID": "tt0080684",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Star Wars: Episode VI - Return of the Jedi",
-      "Year": "1983",
-      "imdbID": "tt0086190",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-    },
-    {
-            "Title": "Star Wars: Episode VII - The Force Awakens",
-            "Year": "2015",
-            "imdbID": "tt2488496",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
-        }
-  ]);
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('')
+  const [favourites, setFavourites] = useState([])
+
+
+  const getMovies = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?&apikey=6122e186`
+
+    const result = await fetch(url)
+    const resultJson = await result.json()
+
+    if (resultJson.Search) {
+      setMovies(resultJson.Search)
+    }
+  }
+
+  useEffect(() => {
+    getMovies(searchValue)
+  }, [searchValue])
+
+  useEffect(() => {
+    const movieFavourites = JSON.parse(
+      localStorage.getItem('react-movie-app-favourites')
+    );
+
+    if (movieFavourites) {
+      setFavourites(movieFavourites)
+    }
+  }, [])
+  
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('react-movie-app-favourites', JSON.stringify(items))
+  }
+
+  const addFavouriteMovie = (movie) => {
+    const newFavouriteList = [...favourites, movie]
+    setFavourites(newFavouriteList)
+    saveToLocalStorage(newFavouriteList)
+  }
+
+  const removeFavouriteMovie = (movie) => {
+    const newFavouriteList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    )
+
+    setFavourites(newFavouriteList)
+    saveToLocalStorage(newFavouriteList)
+  }
+
+
   return (
     <div className='container-fluid movie-app row'>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading='Movies' />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
       <div className='row'>
-        <MovieList movies={movies} />
+        <MovieList
+          movies={movies}
+          handleFavouritesClick={addFavouriteMovie}
+          favouriteComponent={AddFavourites}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading='Favourites' />
+      </div>
+      <div className='row'>
+        <MovieList
+          movies={favourites}
+          handleFavouritesClick={removeFavouriteMovie}
+          favouriteComponent={RemoveFavourites}
+        />
       </div>
       
     </div>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const MovieList = (props) => {
+    const FavouriteComponent = props.favouriteComponent
+
     return (
         <>
             {props.movies.map((movie, index) => (
-                <div className="d-flex justify-content-start m-3 poster">
+                <div className="image-container d-flex justify-content-start m-3 poster">
                     <img src={movie.Poster} alt="Movie"></img>
+                    <div onClick={ ()=> props.handleFavouritesClick(movie) } className="overlay d-flex align-items-center justify-content">
+                        <FavouriteComponent />
+                    </div>
                 </div>
             ))}
         </>
